4047d21a2fae86e4a2c43bf8136909a730e7826f
Sai Zaw Myint <96133665+SaiZawMyint@users.noreply.github.com>
2026-05-14 12:45:08 +0630

    commit latest code

--- src/main/java/org/itech/framework/javafxapp/demo/ApplicationWrapper.java
+++
@@ -1,7 +0,0 @@
-package org.itech.framework.javafxapp.demo;
-
-public class ApplicationWrapper {
-    public static void main(String[] args) throws Exception {
-        TaskManagerApplication.main(args);
-    }
-}
